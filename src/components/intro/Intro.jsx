d9347c7a4d5d5e0977ff259dd295effce3752290
ZsoltTirkala <tirkala.zsolt@gmail.com>
2021-12-30 21:35:46 +0100

    Add intro text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,19 +12,14 @@ export default function Intro() {
       <div className="right">
         <div className="wrapper">
           <h1>Üdvözlöm a Perfectvill oldalán!</h1>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vivamus faucibus, neque et accumsan commodo, odio nulla imperdiet nunc, vel semper ipsum metus quis lorem. 
-            Aliquam a augue euismod, vulputate leo id, vestibulum justo. 
-            Donec sagittis, diam vitae dapibus semper, nulla nibh malesuada urna, nec pellentesque metus purus id neque. 
-            Proin sollicitudin mauris id augue pellentesque, nec aliquam augue interdum. 
-            Vestibulum consequat volutpat sem, sit amet iaculis lacus ultricies vitae. 
-            Nulla ultrices eu ante in finibus. Mauris magna elit, luctus et fermentum non, aliquam eget urna. 
-            Ut vitae ante eu augue euismod pellentesque vitae a nunc. Nullam quis ultrices mauris. 
-            Phasellus felis neque, dignissim fermentum hendrerit quis, 
-            suscipit in urna. Duis purus magna, placerat et diam eu, aliquam scelerisque leo. 
-            Fusce sit amet nibh non nisi dapibus vestibulum sed nec urna. Integer convallis, 
-            diam in ornare luctus, mi nulla vulputate justo, nec tempor eros nisi vitae dolor. 
-            Vestibulum in mattis elit, sit amet tempus tellus.</h3>
+          <h3>Elsősorban lakossági megrendelések teljesítésével, kivitelezési, felújítási és karbantartási munkák
+            elvégzésével foglalkozom, az egyszerű kapcsoló cserétől az új ház teljes villamos rendszerének
+            kialakításáig a jelen kor igényeire szabva. Munkám során kiemelt hangsúlyt fektetek a minőségre és a
+            precízitásra, valamint a megbeszélt vagy szerződésben foglalt határidők pontos betartására. A
+            lehetőségekhez mérten igyekszem összefésülni az esztétikai szempontokat a szakmai szabályokkal, a
+            megrendelői igényekhez igazodva. Amennyiben minőségi villanyszerelést szeretne otthonában, előre
+            egyeztetett rejtett költségek nélküli korrekt áron úgy várom megkeresését elérhetőségeimen telefonon
+            vagy e-mailben messengesren vagy viberen.</h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
