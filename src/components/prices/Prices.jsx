d69983496ffa79f0b9afe1e5c63ed3249e55c2c3
ZsoltTirkala <tirkala.zsolt@gmail.com>
2022-01-14 21:22:10 +0100

    New prices

--- src/components/prices/Prices.jsx
+++ src/components/prices/Prices.jsx
@@ -12,12 +12,44 @@ export default function Prices() {
             <th>Árak</th>
           </tr>
           <tr>
-            <td>Fúrás</td>
-            <td>5000 Ft</td>
+            <td>Lámpa felszerelése-bekötése</td>
+            <td>4000 Ft-tól mérettől és típustól függően</td>
           </tr>
           <tr>
-            <td>Faragás</td>
-            <td>2500 Ft</td>
+            <td>Kapcsoló felszerelése</td>
+            <td>1800 Ft/db</td>
+          </tr>
+          <tr>
+            <td>Dugalj felszerelése</td>
+            <td>1800 Ft/db</td>
+          </tr>
+          <tr>
+            <td>Lakáselosztó cseréje</td>
+            <td>18000 Ft-tól mérettől és típustól függően</td>
+          </tr>
+          <tr>
+            <td>Kismegszakító beszerelés</td>
+            <td>2400 Ft /db</td>
+          </tr>
+          <tr>
+            <td>Életvédelmi relé (Fi-relé) beépítése</td>
+            <td>16000-24000 Ft /db</td>
+          </tr>
+          <tr>
+            <td>Moduláris csengő beépítése</td>
+            <td>3500 Ft/db</td>
+          </tr>
+          <tr>
+            <td>Mozgásérzékelő beépítése</td>
+            <td>4500 Ft/db</td>
+          </tr>
+          <tr>
+            <td>Alkonykapcsoló beépítése</td>
+            <td>4500Ft/db</td>
+          </tr>
+          <tr>
+            <td>Sütő-főzőlap bekötése garanciával</td>
+            <td>10000 –15000 Ft/db</td>
           </tr>
         </tbody>  
     </table>
