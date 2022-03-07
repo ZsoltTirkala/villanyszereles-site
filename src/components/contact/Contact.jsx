cc4568fe15cb4d606357c2c2e3270839624b7ed2
ZsoltTirkala <tirkala.zsolt@gmail.com>
2022-03-07 19:32:35 +0100

    delete obsolate contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,15 +14,7 @@ export default function Contact() {
         <img src="assets/shake.svg" alt="" />
       </div>
       <div className="right">
-        <h2>Kapcsolat.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Név" required="required"/>
-          <input type="text" placeholder="Email" />
-          <input type="tel" pattern='((?:\+?3|0)6)(?:-|\()?(\d{1,2})(?:-|\))?(\d{3})-?(\d{3,4})' id="phone" name="phone" placeholder="+36301234567" required="required" />
-          <textarea placeholder="Üzenet"></textarea>
-          <button type="submit">Küldés</button>
-          {message && <span>Köszönöm! Amint tudom felkeresem!</span>}
-        </form>
+        <h2>Kapcsolat</h2>
       </div>
     </div>
   );
