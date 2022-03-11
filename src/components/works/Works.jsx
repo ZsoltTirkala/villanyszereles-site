14ce2a1ffebe9b4aeefe5f00249aae35ecaa64ed
ZsoltTirkala <tirkala.zsolt@gmail.com>
2022-03-11 21:39:44 +0100

    add new images, delete text above

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -7,78 +7,63 @@ export default function Works() {
     {
       id: "1",
       img:
-        "./img/1.jpg",
+        "./img/1.png",
     },
     {
       id: "2",
       img:
-        "./img/2.jpg",
+        "./img/2.png",
     },
     {
       id: "3",
       img:
-        "./img/3.jpg",
+        "./img/3.png",
     },
     {
       id: "4",
       img:
-        "./img/4.jpg",
+        "./img/4.png",
     },
     {
       id: "5",
       img:
-        "./img/5.jpg",
+        "./img/5.png",
     },
     {
       id: "6",
       img:
-        "./img/6.jpg",
+        "./img/6.png",
     },
     {
       id: "7",
       img:
-        "./img/7.jpg",
+        "./img/7.png",
     },
     {
       id: "8",
       img:
-        "./img/8.jpg",
+        "./img/8.png",
     },
     {
       id: "9",
       img:
-        "./img/9.jpg",
+        "./img/9.png",
     },
     {
       id: "10",
       img:
-        "./img/10.jpg",
+        "./img/10.png",
     },
     {
       id: "11",
       img:
-        "./img/11.jpg",
-    },
-    {
-      id: "12",
-      img:
-        "./img/12.jpg",
-    },
-    {
-      id: "13",
-      img:
-        "./img/13.jpg",
-    },
-    {
-      id: "14",
-      img:
-        "./img/14.jpg",
+        "./img/11.png",
     },
   ];
 
   const handleClick = (way) => {
     way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 13)
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 10)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
   
@@ -91,11 +76,9 @@ export default function Works() {
       >
         {data.map((d) => (
           <div className="container">
-            <p id="img-name">"test"
             <div className="item">
               <img src={d.img} alt="" id="work-image"/>
             </div>
-            </p>
           </div>
         ))}
       </div>
